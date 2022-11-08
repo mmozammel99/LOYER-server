eae391cf293ae885f73ad344df7a9a75cb11d23d
mmozammel99 <mhfaryanemon@gmail.com>
2022-11-08 23:44:39 +0600

    services connect home paget

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 5000
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
 require('dotenv').config()
 
 // middle wares
@@ -26,6 +26,18 @@ async function run() {
             const query = {}
             const cursor = serviceCollection.find(query)
             const service = await cursor.toArray()
+            res.send(service)
+        })
+        app.get('/home', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query)
+            const service = await cursor.limit(3).toArray()
+            res.send(service)
+        })
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const service = await serviceCollection.findOne(query)
             res.send(service)
         })
         app.post('/services', async (req, res) => {
